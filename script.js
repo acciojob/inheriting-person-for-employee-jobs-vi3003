be92ae565f281b8887936646d23d1b7063a8dd98
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-04-21 23:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	greet(){
+		console.log('Hello, my name is ${this.name}, I am ${this.age} years old.')
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) extends Person {
+	jobGreet(){
+		console.log('Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.')
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
